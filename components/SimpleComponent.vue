8e6201b29cc48a08159494ff0668a02c7939f2d3
karlafehir <fehirkarla@gmail.com>
2024-11-06 09:41:41 +0100

    added a simple component with text and img for testing

---
+++ components/SimpleComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="text-section">
+            <h2>Odmor u gradu</h2>
+            <h3>Posebna cijena vikend ponude</h3>
+            <p>Ukupna cijena paketa za 2 osobe i 2 noćenja iznosi 180,70 € za Comfort sobu i 161,20 € za Classic sobu..</p>
+        </div>
+
+        <div class="image-section">
+            <img src="~/assets/hotel_osijek.jpg" alt="Description" />
+        </div>
+    </div>
+</template>
+  
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text-section {
+  flex: 1;
+  padding-right: 20px;
+}
+
+.image-section {
+  width: 300px;
+  height: 600px;
+  overflow: hidden;
+  position: relative;
+}
+
+.image-section img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div>
     <p>Layout with footer and header test</p>
     <Test />
+    <SimpleComponent />
   </div>
 </template>
 
 <script>
 import Test from '~/components/Test.vue'; 
+import SimpleComponent from '../components/SimpleComponent.vue';
 
 export default {
   name: 'IndexPage',
   components: {
-    Test 
+    Test,
+    SimpleComponent
   }
 };
 </script>
